d7125ad8436182d654c876f58bdcc5f1c0c9466b
Arthur <arthurpoplavec01@gmail.com>
2023-09-09 18:07:36 +0300

    Тренировка работы со IntStream

--- src/main/java/org/example/interviews/leetcode/task2/MergingArrays.java
+++ src/main/java/org/example/interviews/leetcode/task2/MergingArrays.java
@@ -1,37 +1,36 @@
 package org.example.interviews.leetcode.task2;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 public class MergingArrays {
     public static void start() {
 
         int [] mass1 = new int[]{3, 4, 5, 6, 7};
         int [] mass2 = new int[]{1, 2, 8, 9, 10};
-        int [] mass = getMass(mass1, mass2);
-        for (int i = 0; i < mass.length; i++) {
-            System.out.println(mass[i]);
-        }
+        //int [] mass = getMassVariant1(mass1, mass2);
+        int [] mass = getMassVariant2(mass1, mass2);
+        IntStream.of(mass).forEach(el -> System.out.print(" " + el));
     }
 
-    public static int [] getMass(int [] mass1, int [] mass2) {
-        //int counter = 0;
-        int [] mass = new int[mass1.length + mass2.length];
+    public static int [] getMassVariant2(int [] mass1, int [] mass2) {
+        return IntStream.concat(Arrays.stream(mass1), Arrays.stream(mass1))
+                .sorted()
+                .toArray();
+    }
+
+    public static int [] getMassVariant1(int [] mass1, int [] mass2) {
+        int size = mass1.length + mass2.length;
         int [] massNew = new int[mass1.length + mass2.length];
-/*
-        for (int i = 0; i < mass1.length; i++) {
-            mass[counter] = mass1[i];
-            counter++;
-        }
-        for (int i = 0; i < mass2.length; i++) {
-            mass[counter] = mass2[i];
-            counter++;
-        }
-        */
-        for (int j = 0; j < mass.length; j++) {
-            for (int l = 1; l < mass.length; l++) {
+        for (int j = 0; j < size; j++) {
+            for (int l = 1; l < size; l++) {
                 if (mass1[j] < mass2[l]) {
                     massNew[j] = mass2[l];
                 }
             }
         }
-        return mass;
+        return massNew;
     }
 }
